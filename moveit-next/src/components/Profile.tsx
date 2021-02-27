783e93db28ce840b74ba5a66b9d84d6a4a01bb2c
carvalhobruno20 <carvalho.bruno20@gmail.com>
2021-02-27 16:25:18 -0300

    dia 4

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/carvalhobruno20.png" alt="Bruno Carvalho" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Bruno Carvalho</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
